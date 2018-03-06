ffa09bc0573a9f7bca358bf3c1fa0f4c3b16bf66
Nguyen Thanh Tam <34813279+tamnguyen311097@users.noreply.github.com>
2018-03-06 14:13:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("Hello Word");
+        }
+    }
+}
